dae6791363a0a0b2e6597de3e315681beec053bf
dscv101 <88686701+dscv101@users.noreply.github.com>
2024-06-24 21:17:40 +0000

    Update default.nix

--- systems/x86_64-linux/blazar/default.nix
+++ systems/x86_64-linux/blazar/default.nix
@@ -3,7 +3,7 @@
 , ...
 }: {
   imports = [ ./hardware-configuration.nix 
-              "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module
+              "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module"
               ./disks.nix ];
 
   # Enable Bootloader
